d82b2e359c58e5537e1fe3b989b77b268bedbabb
Fagner Tranquilino <fagnertds@gmail.com>
2025-01-21 16:49:23 -0300

    GET game by ID

--- src/main/java/com/fagnertds/dslist/services/GameService.java
+++ src/main/java/com/fagnertds/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.fagnertds.dslist.services;
 
+import com.fagnertds.dslist.dto.GameDTO;
 import com.fagnertds.dslist.dto.GameMinDto;
 import com.fagnertds.dslist.entities.Game;
 import com.fagnertds.dslist.repositories.GameRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +17,17 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
         List<Game> result = gameRepository.findAll();
-        List<GameMinDto> dto = result.stream().map(x -> new GameMinDto(x)).toList();
-        return dto;
+        return result.stream().map(x -> new GameMinDto(x)).toList();
+
     }
 }
